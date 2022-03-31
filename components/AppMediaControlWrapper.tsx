d79ba504298fecbc537cac153af40c08ca7b8131
amimaro <amir.zahlan@gmail.com>
2022-03-31 10:45:20 -0300

    Update media control wrapper

--- components/AppMediaControlWrapper.tsx
+++ components/AppMediaControlWrapper.tsx
@@ -1,3 +1,6 @@
+import { useDispatch, useSelector } from "react-redux";
+import { selectIsAuth } from "../store/auth-slice";
+
 type AppMediaControlWrapperProps = {
   children: React.ReactNode;
 };
@@ -5,9 +8,16 @@ type AppMediaControlWrapperProps = {
 export default function AppMediaControlWrapper({
   children,
 }: AppMediaControlWrapperProps) {
+  const dispatch = useDispatch();
+  const isAuth = useSelector(selectIsAuth);
+
   return (
     <div className="absolute -top-1 w-full rounded-full transform -translate-y-1/2">
-      <div className="flex justify-evenly">{children}</div>
+      <div
+        className={`flex ${!isAuth ? "justify-start pl-5" : "justify-evenly"}`}
+      >
+        {children}
+      </div>
     </div>
   );
 }
