75921e88db92008b6c68fc50755150e3a8c315f8
amimaro <amir.zahlan@gmail.com>
2022-03-31 11:27:28 -0300

    Create movies details page

--- pages/movie/[movie_id].tsx
+++ pages/movie/[movie_id].tsx
@@ -1,22 +1,34 @@
+/* eslint-disable @next/next/no-img-element */
+import { format } from "date-fns";
 import type { NextPage } from "next";
 import Head from "next/head";
-import Router, { useRouter } from "next/router";
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { selectIsAuth } from "../../store/auth-slice";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import AppMediaControlItem from "../../components/AppMediaControlItem";
+import AppPopularityIndicator from "../../components/AppPopularityIndicator";
+import AppTitle from "../../components/AppTitle";
+import { getData } from "../../utils/helpers";
 
 const Movie: NextPage = () => {
   const router = useRouter();
   const { movie_id } = router.query;
+  const [movieDetails, setMovieDetails] = useState<any>(null);
 
   const dispatch = useDispatch();
-  const isAuth = useSelector(selectIsAuth);
 
   useEffect(() => {
-    if (!isAuth) {
-      Router.push("/");
-    }
-  }, [isAuth]);
+    (async () => {
+      const movieDetailsRes = await getData(
+        `/api/movie-details?movie_id=${movie_id}`
+      );
+      setMovieDetails(movieDetailsRes);
+    })();
+  }, []);
+
+  if (!movieDetails) {
+    return <div>loading...</div>;
+  }
 
   return (
     <div className="h-full">
@@ -24,7 +36,58 @@ const Movie: NextPage = () => {
         <title>{"Movie - TV shows I've watched"}</title>
       </Head>
 
-      <div className="">Movie: {movie_id}</div>
+      <div>
+        <div className="flex flex-col items-center gap-5">
+          <AppTitle>{movieDetails.title}</AppTitle>
+          <div className="flex md:flex-row flex-col md:items-start items-center gap-5">
+            <img
+              src={`https://image.tmdb.org/t/p/w500${movieDetails.poster_path}`}
+              alt={`${name} poster`}
+              className="rounded-lg w-96"
+            />
+            <div className="flex flex-col px-2 gap-5">
+              <div className="flex flex-col gap-5">
+                <div className="font-semibold">Overview</div>
+                <div className="w-96  text-justify">
+                  {movieDetails.overview}
+                </div>
+                <div className="flex items-center gap-5 font-semibold">
+                  <div>Popularity:</div>
+                  <AppMediaControlItem>
+                    <AppPopularityIndicator
+                      popularity={movieDetails.popularity}
+                    />
+                  </AppMediaControlItem>
+                </div>
+                <div className="flex items-center gap-5 font-semibold">
+                  <div>Release:</div>
+                  <div>
+                    {movieDetails.release_date &&
+                      movieDetails.release_date.length > 0 && (
+                        <div>
+                          {format(
+                            new Date(movieDetails.release_date),
+                            "MMM dd, yyyy"
+                          )}
+                        </div>
+                      )}
+                  </div>
+                </div>
+                <div className="flex flex-wrap gap-5 w-80">
+                  {movieDetails.genres.map((genre: any) => (
+                    <div
+                      key={genre.id}
+                      className="text-white bg-teal-800 py-2 px-4 rounded-full font-semibold"
+                    >
+                      {genre.name}
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
